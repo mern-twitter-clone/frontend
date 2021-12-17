66b8923afd9b9ff9c32820ff1fd0c8af3d3d9aef
Ofek Itzhaki <ofeeki111@gmail.com>
2021-12-17 19:22:14 +0200

    changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import './App.css';
 const myfirstelement = <h1>Hello React!</h1>
 
 ReactDOM.render(myfirstelement, document.getElementById('root'));
