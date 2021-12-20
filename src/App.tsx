967a4a3592c53b87dc3d5926562097ab33d26519
Ilia Kamilov <iliakmlv@gmail.com>
2021-12-20 12:18:11 +0200

    es6 migration

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,8 @@ import React from 'react';
 import './App.css';
 import Main from './Components/Main/Main';
 
-
-function App() {
-  return (
-    <div className="App">
-     <Main></Main>
-    </div>
-  );
+const App: React.FC = () => {
+  return <Main />
 }
 
 export default App;
